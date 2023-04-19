c257492d6a60d55a35d3ca178901ecb2745828c0
ashton8504 <ashtonlittle8504@gmail.com>
2023-04-19 14:06:41 -0400

    saving step one as it is working

--- guessing-game.js
+++ guessing-game.js
@@ -1,1 +1,17 @@
-console.log("test");
+// Step 1
+
+let secretNumber = 23;
+
+// Step 2
+
+function checkGuess(num) {
+  if (num > secretNumber) {
+    console.log("Too high");
+  } else if (num < secretNumber) {
+    console.log("Too low");
+  } else if (num === secretNumber) {
+    console.log("Correct");
+  }
+}
+
+console.log(checkGuess(23));
