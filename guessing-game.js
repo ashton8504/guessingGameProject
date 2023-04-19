31fade1e48f0acfbc91b7645a6941738c8a59225
ashton8504 <ashtonlittle8504@gmail.com>
2023-04-19 14:56:29 -0400

    saving

--- guessing-game.js
+++ guessing-game.js
@@ -5,7 +5,13 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-let secretNumber = 23;
+let secretNumber = randomInRange(1, 10);
+
+function randomInRange(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
 
 function checkGuess(num) {
   if (num > secretNumber) {
